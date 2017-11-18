900879b15d61844b71b6a724a1a2add6d13d2a75
Naufal Fachrian <naufal@docotel.co.id>
2017-11-18 12:02:23 +0700

    replace app name on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name', 'Laravel') }}
                 </div>
 
                 <div class="links">
